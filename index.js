c909ec5297ba1b0fc896646b87e39d9583fe0990
jkeslar21 <diamondroad91@gmail.com>
2019-03-29 10:32:06 -0700

    stretchCRUDs

--- index.js
+++ index.js
@@ -45,6 +45,23 @@ const knexConfig = {
         res.status(500).json(err)
     })
 })
+
+server.get('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    db('projects')
+    .where({ id })
+    .first()
+    .then(project => {
+        if (project) {
+          res.status(200).json(project)
+        } else {
+          res.status(404).json({ message: 'Project Not Found'})
+        }
+      })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+  })
   
 server.get('/api/projects/:id/actions', (req, res) => {
     const id = req.params.id
@@ -57,9 +74,40 @@ server.get('/api/projects/:id/actions', (req, res) => {
       })
       .catch(err  => res.status(500).json(err))
   })
+  
+  server.delete('/api/projects/:id', (req, res) => {
+    db('projects')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(204).end()
+      } else {
+        res.status(404).json({ message: 'Not Found'})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Request Failed' })
+    })
+})
+
+server.put('/api/projects/:id', (req, res) => {
+    db('projects')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count)
+      } else {
+        res.status(404).json({ message: 'Project Not Found'})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Request Failed'})
+    })
+  })
 
 // Action Routes
-
   server.post('/api/actions', (req, res) => { 
     db('actions')
     .insert(req.body)
@@ -78,6 +126,55 @@ server.get('/api/actions', (req, res) => {
         res.status(500).json(err)
     })
 })
+
+server.get('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    db('actions')
+    .where({ id })
+    .first()
+    .then(action => {
+        if (action) {
+          res.status(200).json(action)
+        } else {
+          res.status(404).json({ message: 'Project Not Found'})
+        }
+      })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+  })
+
+server.delete('/api/actions/:id', (req, res) => {
+    db('actions')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(204).end()
+      } else {
+        res.status(404).json({ message: 'Not Found'})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Request Failed' })
+    })
+})
+
+server.put('/api/actions/:id', (req, res) => {
+    db('actions')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count)
+      } else {
+        res.status(404).json({ message: 'Project Not Found'})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Request Failed'})
+    })
+  })
 
 server.listen(2100, () =>
   console.log(`\n** Jungle on http://localhost:2100 **\n`)
